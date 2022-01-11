01b64b4077a7f6c4f812946d2eb00858bdb9110c
Lucy Lee <gamesandhannah@gmail.com>
2022-01-11 04:33:44 -0500

    Switch to using a table to structure cells

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const colorGrid = document.getElementById("color-grid");
 
+let rows = 0;
+let cols = 0;
+
+let row = document.createElement("tr");
+let col = document.createElement("th");
+
+let cell = document.createElement("span");
+cell.classList.add("cell");
+
 function addRow() {
-    colorGrid.innerHTML += `<div class="cell"></div>`
+    colorGrid.insertRow(-1);
+    rows++;
+
+    for (let i = 0; i <= cols; i++) {
+        colorGrid.insertCell(i)
+    }
 }
+
+function addCol() {
+    colorGrid.appendChild(cell)
+    col++;
+
+    for (let i = 0; i <= rows; i++) {
+        col.appendChild(row);
+    }
+}
+
