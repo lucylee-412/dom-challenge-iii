c5b36d659217335214da1f9f8da637999cbbf723
Lucy Lee <gamesandhannah@gmail.com>
2022-01-11 09:55:16 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 const colorGrid = document.getElementById("color-grid");
 
-let rowCount = 0;
-let colCount = 0;
+let rowCount = 0; // # of displayed rows
+let colCount = 0; // # oof displayed cols
 
-let row = document.getElementsByTagName("tr");
+/* Nonworking code
+let col = document.createElement("th")
+let cell = document.createElement("span")
+cell.classList.add("cell")
+col.appendChild(cell)
+*/
 
+let row = document.createElement("tr");
+let col = document.createElement("th");
+
+// User can add a row by clicking "Add Row" text on navigation menu
 
 function addRow() {
+    colorGrid.insertRow(-1);
+    rowCount++;
+
+    for (let i = 0; i <= colCount; i++) {
+        colorGrid.insertCell(i)
+    }
+
+/* Nonworking code
     let newRow = colorGrid.insertRow(-1);
     rowCount++;
 
     for (let i = 0; i <= colCount; i++) {
-        newRow.insertCell(i)
+        col = newRow.insertCell(i)
+        cell = document.createElement("span")
+        cell.classList.add("cell")
+        col.appendChild(cell)
     }
+ */
 }
 
+// User cam add a column by click "Add Column" text on navigation menu
+
 function addCol() {
+    /* Nonworking code
+    if (rowCount === 0) {
+        addRow()
+    }
     colCount++;
 
-    for (let i = 0; i <= colorGrid.rows.length; i++) {
-        newRow.insertCell(colCount);
-    }
+    let row = document.getElementsByTagName("tr");
+
+    //for (let i = 0; i <= row.length; i++) {
+     //   row[i].appendChild(col)
+    //}
+    */
 }
 
