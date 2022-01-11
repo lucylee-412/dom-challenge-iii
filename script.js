e56d0659a4fb97990337b7e85ded0a14c842fbb7
Lucy Lee <gamesandhannah@gmail.com>
2022-01-11 13:47:11 -0500

    Modify addCol() to use an Array.from iterator

--- script.js
+++ script.js
@@ -1,29 +1,46 @@
 const colorGrid = document.getElementById("color-grid");
 
-let rows = 0;
-let cols = 0;
+let rowCount = 0; // # of displayed rows
+let colCount = 0; // # of displayed cols
 
-let row = document.createElement("tr");
-let col = document.createElement("th");
+//let row = document.createElement("tr");
+let col = document.createElement("td");
 
-let cell = document.createElement("span");
-cell.classList.add("cell");
+
+
+
+// User can add a row by clicking "Add Row" text on navigation menu
 
 function addRow() {
-    colorGrid.insertRow(-1);
-    rows++;
+    let newRow = colorGrid.insertRow(-1);
+    rowCount++;
 
-    for (let i = 0; i <= cols; i++) {
-        colorGrid.insertCell(i)
+    for (let i = 0; i <= colCount; i++) {
+        newRow.insertCell(i)
+        let col = document.getElementsByTagName("td");
+        let cell = document.createElement("span")
+        cell.classList.add("cell")
+        Array.from(col, i => i.appendChild(cell));
     }
 }
 
-function addCol() {
-    colorGrid.appendChild(cell)
-    col++;
+// User cam add a column by click "Add Column" text on navigation menu
 
-    for (let i = 0; i <= rows; i++) {
-        col.appendChild(row);
+function addCol() {
+    if (rowCount === 0) {
+        addRow()
+        return
+    }
+    colCount++;
+
+    for (let i = 0; i <= rowCount; i++) {
+        let row = document.getElementsByTagName("tr");
+        let col = document.createElement("td");
+        let cell = document.createElement("span")
+        cell.classList.add("cell")
+        col.appendChild(cell)
+        console.log(col)
+        Array.from(row, i => i.appendChild(col));
     }
 }
 
